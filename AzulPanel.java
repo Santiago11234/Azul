414b4650ecfa297f554646c1618a51d9cd92f10d
Vnakkala30 <vnakkala30@gmail.com>
2022-11-08 13:14:20 -0600

    two

---
+++ AzulPanel.java
@@ -0,0 +1,69 @@
+
+	
+	import java.awt.*;
+	import java.awt.image.*;
+	import javax.imageio.ImageIO;
+	import javax.swing.*;
+	import java.io.*;
+	import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.awt.Color;
+	import java.awt.Graphics;
+	import javax.swing.JComponent;
+	public class  AzulPanel extends JPanel implements MouseListener {
+	
+	
+	
+	
+	
+	public AzulPanel (){ 
+	       try {
+	        	        
+	           
+	       }
+	       
+	       catch ( Exception E) {
+	        System.out.println("This is a Exception Error" + E.getMessage());
+	        return;
+	       }
+	       
+	 
+	     
+	     
+	     
+	}
+
+
+
+	public void mousePressed(MouseEvent e) { }
+	public void mouseReleased(MouseEvent e) { }
+	public void mouseEntered(MouseEvent e) { }
+	public void mouseExited(MouseEvent e) { }
+	public void mouseClicked (MouseEvent e )
+	{
+		
+	}
+
+
+	public void paint (Graphics g) {
+	      
+		// get Random Start Player * This will be in constructor assign it to this int
+		
+	 // if ( Draw Boards = true ) - draw the fucking boards 
+		   // if ( BoardNum == 1 && FactoryT
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+	
+
