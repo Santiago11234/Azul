349da7593b2b0459a7c6a7d9f9018c06fcccad3e
Vnakkala30 <vnakkala30@gmail.com>
2022-11-09 13:00:52 -0600

    commit

---
+++ AzulFrame.java
@@ -0,0 +1,33 @@
+
+
+
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+public class AzulFrame extends JFrame {
+	private static final int WIDTH = 1600;
+	private static final int HEIGHT = 960;
+	
+	public AzulFrame (String title) {
+			super(title);
+			setSize(WIDTH,HEIGHT);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			add( new AzulPanel());
+			setVisible(true);
+			
+	}
+
+
+
+	//@Override
+	//public void actionPerformed(ActionEvent e) {
+		//// TODO Auto-generated method stub
+		
+	//}
+}
+
+
+
