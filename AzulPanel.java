38558f486adb940233e9c7b5fb014809b88bf289
K1206505 <kevthink123@gmail.com>
2022-11-10 12:59:16 -0600

    format change + testing commit

--- AzulPanel.java
+++ AzulPanel.java
@@ -1,20 +1,19 @@
-
-	
-	import java.awt.*;
-	import java.awt.image.*;
-	import javax.imageio.ImageIO;
-	import javax.swing.*;
-	import java.io.*;
-	import java.awt.event.MouseListener;
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-	import java.util.ArrayList;
-	import java.util.Collections;
-	import java.awt.Color;
-	import java.awt.Graphics;
-	import javax.swing.JComponent;
-	public class  AzulPanel extends JPanel implements MouseListener {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+public class  AzulPanel extends JPanel implements MouseListener {
 	
 	
 	
