29e7ea211677c0b7b20fa0c2adc9407a79122a99
Vnakkala30 <vnakkala30@gmail.com>
2022-11-14 13:13:18 -0600

    Commiting Buttons

--- AzulPanel.java
+++ AzulPanel.java
@@ -1,67 +1,75 @@
-import java.awt.*;
-import java.awt.image.*;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.io.*;
-import java.awt.event.MouseListener;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.awt.Color;
-import java.awt.Graphics;
-import javax.swing.JComponent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.awt.*;
+import java.util.*;
 
-public class  AzulPanel extends JPanel implements MouseListener {
-	
-	private BufferedImage board,home;
-	
-	
-	
-	public AzulPanel (){ 
-	       try {
-	        
-            board = ImageIO.read(new File("Board.jpg"));
-	        home = ImageIO.read(new File("AzulHome.png"));
-	       }
-	       
-	       catch ( Exception E) {
-	        System.out.println("This is a Exception Error" + E.getMessage());
-	        return;
-	       }
-	       
-	 
-	     
-	     
-	     
-	}
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
-	public void paint (Graphics g) {
-	    
-		g.drawImage(home,0,0,1600,960, null);
-        //g.drawImage( board , 500 , 200 , 400 , 400 , null);
-
-	}
-
-
+public class AzulPanel extends JPanel implements ActionListener , MouseListener  {
+	private BufferedImage home , board;
+	private JButton button1;
+	private boolean step1 , step2;	
+    public AzulPanel() {
+    	try {
+    		home = ImageIO.read(new File("AzulHome.png"));
+    		board = ImageIO.read(new File("Board.jpg"));
+    	}
+    	
+    	catch (Exception E) {
+    		System.out.println("This is a Exception Error" + E.getMessage());
+    		return;
+    	}
+    	
+    	button1 = new JButton(" 1 Player ");
+    	button1.setSize(100, 80);
+    	button1.setLocation(577 , 482);
+    	this.setLayout(null);
+    	this.add(button1);
+    	button1.addActionListener(this);
+    	addMouseListener(this);
+    	button1.setVisible(true);
+    	step1 = true;
+    	step2 = false;
+    	
+     
+    	
+    	
+    }
 	public void mousePressed(MouseEvent e) { }
 	public void mouseReleased(MouseEvent e) { }
 	public void mouseEntered(MouseEvent e) { }
 	public void mouseExited(MouseEvent e) { }
 	public void mouseClicked (MouseEvent e )
 	{
-		int xc = e.getX();
-		int yc = e.getY();
-		System.out.println("loc is ("+xc+", "+yc+")");
 	}
-
-
-	
-	
-	
-	
+        public void paint (Graphics g) {
+        			super.paint(g);
+        			if (step1 == true) {
+        	        g.drawImage(home, 500 , 0 , 450 , 600 , null);
+        	        step2 = true;
+        	        step1 = false;
+        			}
+        			
+        			else if ( step2 == true) {
+        				g.drawImage(board , 0 , 500 , 300 , 450 , null);
+        			}
+        			
+        	        
+        }
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getSource() == button1) {
+				  step2 = true;
+			      repaint();
+			}
+		}
 	
 }
-	
-
